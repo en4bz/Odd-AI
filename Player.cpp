7df99b5f5eace6a97a6ec54db917539d2369b5db
Ian Forbes <ian.forbes@mail.mcgill.ca>
2013-03-20 16:00:00 -0400

    Add Round Counter

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,15 @@
 #include "Player.hpp"
 
+int Player::roundNumber(void) const{
+    return this->mRound;
+}
+
+void Player::updateBoard(const Move& v){
+    this->mRound++;
+    this->mCurrentState[v.place] = v.colour;
+}
+
 void Player::updateBoard(const Point& p, Board::VALUE v){
+    this->mRound++;
     this->mCurrentState[p] = v;
 }
--- Player.hpp
+++ Player.hpp
@@ -1,14 +1,18 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
+#include "Move.hpp"
 #include "Point.hpp"
 #include "Board.hpp"
 
 class Player{
 protected:
+	int mRound;
 	Board mCurrentState;
 public:
+    int roundNumber(void) const;
     void updateBoard(const Point& newPoint, Board::VALUE);
+    void updateBoard(const Move& m);
     virtual Point move(void) = 0;
 };
 #endif
