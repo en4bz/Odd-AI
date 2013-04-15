17f21fd891e9e750049773553b909b585c9338f5
Ian Forbes <ian.forbes@mail.mcgill.ca>
2013-04-15 01:17:24 -0400

    Fixed big bug in Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,8 @@
 #include "Player.hpp"
 
-Player::Player(int pID) : mID(pID), mGoal(pID == 1 ? Board::STATE::ODD : Board::STATE::EVEN), mCurrentState(new Board()) {}
+Player::Player(int pID) : mID(pID), mGoal(pID == 1 ? Board::STATE::ODD : Board::STATE::EVEN), mRound(0), mCurrentState(new Board()) {}
 
-Player::Player(int pID, Board* const pBoard) : mID(pID), mGoal(pID == 1 ? Board::STATE::ODD : Board::STATE::EVEN), mCurrentState(pBoard) {}
+Player::Player(int pID, Board* const pBoard) : mID(pID), mGoal(pID == 1 ? Board::STATE::ODD : Board::STATE::EVEN), mRound(0), mCurrentState(pBoard) {}
 
 int Player::roundNumber(void) const{
 	return this->mRound;
