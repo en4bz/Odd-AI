1f752bacfa65d6fb5e8fbd0056c6b50db79f3f0f
Ian Forbes <ian.forbes@mail.mcgill.ca>
2013-04-20 00:03:55 -0400

    Added Release macro.

--- Point.cpp
+++ Point.cpp
@@ -1,28 +1,16 @@
 #include "Point.hpp"
 
 Point::Point(int px, int py) : x(px), y(py){
+	#ifndef RELEASE
 	if(abs(px + py) > 4 || abs(px) > 4 || abs(py) > 4){
 		throw BoardBoundsException(px,py);
 	}
+	#endif
 }
 
 bool Point::operator < (const Point& pToCompare) const{
     return (this->x < pToCompare.x) || ((this->x == pToCompare.x) && (this->y < pToCompare.y));
 }
-/*
-bool Point::operator < (const Point& pToCompare) const{
-	if(this->x < pToCompare.x){
-		return true;
-	}
-	else if(this->x == pToCompare.x){
-		return this->y > pToCompare.y;
-	}
-	else{
-		return false;
-	}
-}*/
-
-
 
 bool Point::operator== (const Point& pToCompare) const{
 	return x == pToCompare.x && y == pToCompare.y;
